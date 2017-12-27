165158c2ac699c0e530bad79366b241ee0abccea
Josiah DuBose <josiah7686@gmail.com>
2017-12-27 08:54:34 -0500

    revamp in progress

--- js/index.js
+++ js/index.js
@@ -1,27 +1,44 @@
-$(".container-header").height($(window).height());
+$(document).ready(function() {
+    $("#loadBio, #nav-bio").click(loadBio);
+    $("#loadResume, #nav-resume").click(loadResume);
+    $("#loadProjects, #nav-projects").click(loadProjects);
+    $("#home").show();
+    $("#nav-home").click(() => {
+        $(".container-fluid").hide();
+        $("#home").show();
+        $(".nav").hide();
+    });
+    $("#contact-pop").click(function() {
+        $(".navbar-nav").find("li.active").each(function(index, element){
+            $(element).removeClass("active");
+        });
+        $(".nav-item.contact").addClass(" active");
+    });
+});
 
-$("#resume").width($(window).width());
-
-function scrollResume(){
-    $('html, body').animate({
-        scrollTop: $("#resume_section").offset().top - 5
-    }, 1);
+function loadBio() {
+    prepPage();
+    $(".nav-item.bio").addClass(' active');
+    $(".container-fluid").load('partials/bio.html');
 }
-$("#resumeButton").click(scrollResume)
-$("#resumebutton").click(scrollResume)
 
-
-function scrollPortfolio(){
-    $('html, body').animate({
-        scrollTop: $("#portfolio_section").offset().top
-    }, 1);
+function loadResume() {
+    prepPage();
+    $(".nav-item.resume").addClass(' active');
+    $(".container-fluid").load('partials/resume.html');
 }
-$("#projects").click(scrollPortfolio)
 
-
-function scrollBio(){
-    $('html, body').animate({
-        scrollTop: $("#about").offset().top
-    }, 1);
+function loadProjects() {
+    prepPage();
+    $(".nav-item.projects").addClass(' active');
+    $(".container-fluid").load('partials/projects.html');
 }
-$("#bioButton").click(scrollBio)
+
+function prepPage() {
+    $("#home").hide();
+    $(".nav").show();
+    $(".navbar-nav").find("li.active").each(function(index, element){
+        $(element).removeClass("active");
+    });
+    $(".container-fluid").empty().show();
+}
