45dccd854f12f0d4b592219a92f5eab2a106866e
Josiah DuBose <josiah7686@gmail.com>
2019-11-19 09:59:16 -0500

    page title

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     $("#loadResume, #nav-resume").click(loadResume);
     $("#loadProjects, #nav-projects").click(loadProjects);
     $("#home").show();
+    $(document).prop('title', 'Home');
     $("#nav-home").click(() => {
         $(".container-fluid").hide();
         $("#home").show();
@@ -11,13 +12,13 @@ $(document).ready(function() {
 });
 
 function loadBio() {
-    prepPage();
+    prepPage('Bio');
     $(".nav-item.bio").addClass(' active');
     $(".container-fluid").load('partials/bio.html');
 }
 
 function loadResume() {
-    prepPage();
+    prepPage('Resume');
     $(".nav-item.resume").addClass(' active');
     $(".container-fluid").load('partials/resume.html', function() {
         $("#resume").height($(window).height());
@@ -25,13 +26,14 @@ function loadResume() {
 }
 
 function loadProjects() {
-    prepPage();
+    prepPage('Projects');
     $(".nav-item.projects").addClass(' active');
     $(".container-fluid").load('partials/projects.html');
 }
 
-function prepPage() {
+function prepPage(page) {
     $("#home").hide();
+    $(document).prop('title', page);
     $(".nav").show();
     $(".navbar-nav").find("li.active").each(function(index, element){
         $(element).removeClass("active");
